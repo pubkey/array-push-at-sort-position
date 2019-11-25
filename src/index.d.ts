681c1604424e8a41a88280a5241fb170110e0975
pubkey <daniel_van_meyer@hotmail.de>
2019-11-25 18:25:13 +0100

    FIX typing

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
-export type CompareFunction<T> = (a: T, b: T) => 1 | 0 | -1;
+export type CompareFunction<T = any> = (a: T, b: T) => 1 | 0 | -1;
 
 export function pushAtSortPosition<T>(
     array: T[],

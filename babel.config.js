6148e1360da4f5d83831d47256230b96706420a6
pubkey <8926560+pubkey@users.noreply.github.com>
2023-10-09 02:24:01 +0200

    4.0.1

--- babel.config.js
+++ babel.config.js
@@ -15,7 +15,6 @@ if (process.env['NODE_ENV'] === 'es5') {
                 firefox: '60',
                 chrome: '67',
                 safari: '11.1',
-                ie: '11'
             },
             useBuiltIns: false
         }]
